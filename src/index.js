51cf0a418707d3ab4bb16195426562db3a177741
23020660 Nguyễn Văn Hoàng Hải <23020660@vnu.edu.vn>
2026-09-02 00:19:03 +0700

    refactor(app): remove legacy compiled javascript files and deprecated commands

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RagChatApp = void 0;
-var RagChatApp_1 = require("../RagChatApp");
-Object.defineProperty(exports, "RagChatApp", { enumerable: true, get: function () { return RagChatApp_1.RagChatApp; } });
